128f3d7e1f7a5ebff0c0eda838376ceffe67b5e5
guimendel <guimendeln@gmail.com>
2021-10-12 22:14:07 -0300

    feat/ add aimator sync

--- Assets/Scripts/Characters/HoundAnimatorSync.cs
+++ Assets/Scripts/Characters/HoundAnimatorSync.cs
@@ -2,17 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Deps
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(GroundMovement))]
+
 public class HoundAnimatorSync : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  // Refs
+  Animator _animator;
+  Rigidbody2D _rigidbody;
+  GroundMovement _groundMovement;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  private void Awake()
+  {
+    _animator = GetComponent<Animator>();
+    _rigidbody = GetComponent<Rigidbody2D>();
+    _groundMovement = GetComponent<GroundMovement>();
+  }
+
+  private void Update()
+  {
+    DetectMovement();
+    DetectAirborne();
+  }
+
+  private void DetectMovement()
+  {
+    // Detect x movement (exerted by the hound, not external entities)
+    float xMovement = Mathf.Abs(_groundMovement.GetLastFrameMovement());
+
+    // Update animation
+    _animator.SetBool("Running", xMovement > 0.05f);
+  }
+
+  private void DetectAirborne()
+  {
+    // Detect y movement
+    float yMovement = Mathf.Abs(_rigidbody.velocity.y);
+
+    // Detect significant vertical movement
+    _animator.SetBool("Airborne", yMovement > 0.1f);
+  }
 }
